6f292336a8cb25fd0e43b79d7c83f320c8958704
23915 <minxiang@gmail.com>
2026-03-31 09:28:39 +0800

    修改部分代码

--- backend/src/main/java/com/gongxifacai/gongxifacai/controller/UserController.java
+++ backend/src/main/java/com/gongxifacai/gongxifacai/controller/UserController.java
@@ -5,8 +5,6 @@ import com.gongxifacai.gongxifacai.entity.Holding;
 import com.gongxifacai.gongxifacai.service.UserService;
 import com.gongxifacai.gongxifacai.service.impl.UserServiceImpl.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,21 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/Users")
+@RequestMapping("/api/v1/users")
 public class UserController {
     @Autowired
     private   UserService userService;
-
-
-//    获取用户信息
+    //    获取用户信息
     @GetMapping("/{id}")
-    public ResponseEntity<UserInfo> getUserInfo(@PathVariable Long id) {
-
-        return ResponseEntity.ok(userService.getUserInfo(id));
+    public Result<UserInfo> getUserInfo(@PathVariable Long id) {
+        return Result.success(userService.getUserInfo(id));
     }
-//获取用户持仓情况
-    @GetMapping("/{id}/Holdings")
-    public ResponseEntity<List<Holding>> getUserHoldings(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.getUserHoldings(id));
+    //获取用户持仓情况
+    @GetMapping("/{id}/holdings")
+    public Result<List<Holding>> getUserHoldings(@PathVariable Long id) {
+        return Result.success(userService.getUserHoldings(id));
     }
 }
